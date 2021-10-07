30f0e8ea67f4a675986b505d715316100eb0e38b
Facundo Graña <F_E_G.93@hotmail.com>
2021-10-07 00:00:15 -0300

    prueba con port no fijo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import * as express from "express";
 import { nanoid } from "nanoid";
 //API INIT AND CONFIG
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 const userCollectionRef = firestore.collection("users");
--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import { nanoid } from "nanoid";
 
 //API INIT AND CONFIG
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 app.use(express.json());
 app.use(cors());
 
